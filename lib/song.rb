9ca6cdd3641252c856375fad2d0ac51c8585c67e
Khang <khang.le27@gmail.com>
2019-02-21 00:11:18 +0000

    class methods and abstraction for Song class

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,59 @@ class Song
     self.class.all << self
   end
 
+  def self.create #(name, artist)
+    song_instance = self.new
+    # song.name = name
+    # song.artist = artist
+    self.all << song_instance
+    song_instance
+  end
+
+  def self.new_by_name(name)
+    song_instance = self.new
+    song_instance.name = name
+    song_instance
+  end
+
+  def self.create_by_name(name)
+    song_instance = self.new_by_name(name)
+    self.all << song_instance
+    song_instance
+  end
+
+  def self.find_by_name(researched_name)
+    self.all.find {|song_instance| song_instance.name == researched_name} #return the matching song_instance or false
+  end
+
+  def self.find_or_create_by_name(researched_name)
+    if self.find_by_name(researched_name) != nil
+      self.find_by_name(researched_name)
+    else
+      self.create_by_name(researched_name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song_instance| song_instance.name}
+  end
+
+  def self.new_from_filename(song_mp3)
+    song_instance = self.new
+    array = song_mp3.split(" - ")
+    song_instance.name = array[1].chomp(".mp3")
+    song_instance.artist_name = array[0]
+    song_instance
+  end
+
+  def self.create_from_filename(song_mp3)
+    song_instance = self.new_from_filename(song_mp3)
+    self.all << song_instance
+    song_instance
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+
 end
