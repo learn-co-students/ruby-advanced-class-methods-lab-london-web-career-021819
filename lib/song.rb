8171603c9c9766332fc2cd5b984a4f43adca6742
github username <github email address>
2019-02-21 15:28:05 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +10,18 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    name = name
+    song = self.new
+    song.name = name
   end
 
 end
